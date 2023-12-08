6078f65409b309b2ef0f525d027c807add07747f
Bernardo <bgrodrigues2005@gmail.com>
2023-12-07 23:17:50 -0300

    comeco da autenticacao

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import routerUser from './users/users-controller.js';
 import routerComment from './comments/comments-controller.js';
 import routerPost from './posts/posts-controller.js';
+import authRouter from './auth/auth.controller.js';
 
 const app = express();
 app.use(express.json());
