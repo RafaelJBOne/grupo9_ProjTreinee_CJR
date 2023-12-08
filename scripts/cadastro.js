a6da6ea387db6eddc50910f064dc5ba5ed4b9db8
vini-fmo <vinicius.fmo16@gmail.com>
2023-12-08 01:58:56 -0300

    identacao

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -1,25 +1,25 @@
-    const email = document.getElementById('email').value;
-    const senha = document.getElementById('senha').value;
-    const nome = document.getElementById('nome').value;
-    const genero = document.getElementById('genero').value;
-    const cargo = document.getElementById('cargo').value;
+const email = document.getElementById('email').value;
+const senha = document.getElementById('senha').value;
+const nome = document.getElementById('nome').value;
+const genero = document.getElementById('genero').value;
+const cargo = document.getElementById('cargo').value;
 
-    const dadosUsuario = {
-        email: email,
-        senha: senha,
-        nome: nome,
-        genero: genero,
-        cargo: cargo
-    };
+const dadosUsuario = {
+    email: email,
+    senha: senha,
+    nome: nome,
+    genero: genero,
+    cargo: cargo
+};
 
-    // Enviar uma solicitação POST para o backend
-    fetch('http://localhost:3000/cadastro', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(dadosUsuario)
-    })
+// Enviar uma solicitação POST para o backend
+fetch('http://localhost:3000/cadastro', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(dadosUsuario)
+})
     .then(response => response.json())
     .then(data => {
         // Lidar com a resposta do backend
