7d7d01e522c2c19ae1dea1725ab7791b76694fe8
bg <bgrodrigues2005@gmail.com>
2023-12-09 23:00:49 -0300

    auth 1

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -1,31 +1,40 @@
-const email = document.getElementById('email').value;
-const senha = document.getElementById('senha').value;
-const nome = document.getElementById('nome').value;
-const genero = document.getElementById('genero').value;
-const cargo = document.getElementById('cargo').value;
+document.addEventListener('DOMContentLoaded', () => {
+    const signUpForm = document.querySelector('.fm');
 
-const dadosUsuario = {
-    email: email,
-    senha: senha,
-    nome: nome,
-    genero: genero,
-    cargo: cargo
-};
+    signUpForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
 
-// Enviar uma solicitação POST para o backend
-fetch('http://localhost:3000/cadastro', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(dadosUsuario)
-})
-    .then(response => response.json())
-    .then(data => {
-        // Lidar com a resposta do backend
-        console.log('Resposta do backend:', data);
-        // Redirecionar ou realizar outras ações conforme necessário
-    })
-    .catch(error => {
-        console.error('Erro ao enviar a solicitação:', error);
+        const username = document.getElementById('nome').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('senha').value;
+        const job_title = document.getElementById('cargo').value;
+        const gender = document.getElementById('genero').value;
+
+        try {
+            const response = await fetch('http://localhost:3000/sign-up', {
+                mode: 'no-cors',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username,
+                    email,
+                    password,
+                    job_title,
+                    gender,
+                }),
+            });
+
+            if (response.ok) {
+                // Handle successful registration (redirect or show a success message)
+                console.log('Registration successful!');
+            } else {
+                const data = await response.json();
+                console.error(`Registration failed: ${data.message}`);
+            }
+        } catch (error) {
+            console.error('Error during registration:', error.message);
+        }
     });
+});
