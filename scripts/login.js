0f21153982ce9252b267e8349b109cdb90ee02a1
bg <bgrodrigues2005@gmail.com>
2023-12-10 05:15:30 -0300

    login start 2

--- scripts/login.js
+++ scripts/login.js
@@ -1,40 +1,34 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.getElementById('camposLogin');
 
-document.addEventListener('DOMContentLoaded', () => {
-    const signUpForm = document.querySelector('.fm');
-
-    signUpForm.addEventListener('submit', async (event) => { 
+    loginForm.addEventListener('submit', async function (event) {
         event.preventDefault();
 
-        const username = document.getElementById('nome').value;
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('senha').value;
-        const job_title = document.getElementById('cargo').value;
-        const gender = document.getElementById('genero').value;
+        const email = document.getElementById('loginInput').value;
+        const password = document.getElementById('senhaInput').value;
 
-        try {            
-            const response = await fetch('http://localhost:3000/sign-up', { // cadastrar usuário
+        try {
+            const response = await fetch('/api/auth/sign-in', {
                 method: 'POST',
                 headers: {
-                    "Content-Type": "application/json",},
-                body: JSON.stringify({
-                    username,
-                    email,
-                    password,
-                    job_title,
-                    gender,
-                    admin: false,
-                }),
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email, password }),
             });
 
-            if (response.ok) {
-                // Handle successful registration (redirect or show a success message)
-                console.log('Registration successful!');
-            } else {
-                const data = await response.json();
-                console.error(`Registration failed: ${data.message}`);
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData.error || 'Failed to log in');
             }
+
+            const token = await response.json();
+            localStorage.setItem('token', token);
+            // You can redirect to another page or perform other actions after successful login
+            window.location.href = '/dashboard'; // Replace with the desired URL
+
         } catch (error) {
-            console.error('Error during registration:', error.message);
+            console.error(error);
+            // Handle the error, e.g., show an error message to the user
         }
     });
 });
